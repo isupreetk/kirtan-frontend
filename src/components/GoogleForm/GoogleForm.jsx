71359ed8617af4a4eaf2b56d0b3b237041f5ed83
supreetkaur <supreet97@gmail.com>
2023-12-27 11:49:29 -0800

    google form in new tab

--- src/components/GoogleForm/GoogleForm.jsx
+++ src/components/GoogleForm/GoogleForm.jsx
@@ -3,7 +3,10 @@ import "./GoogleForm.scss";
 function GoogleForm() {
   return (
     <div className="google-form">
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLScokLn5BA3IjiXZ37g1oaY7cWl7_bx4RzLCbFNcSa3XspcS0w/viewform?usp=sf_link">
+      <a
+        target="_blank"
+        href="https://docs.google.com/forms/d/e/1FAIpQLScokLn5BA3IjiXZ37g1oaY7cWl7_bx4RzLCbFNcSa3XspcS0w/viewform?usp=sf_link"
+      >
         Questions/feedback? Click here
       </a>
     </div>
