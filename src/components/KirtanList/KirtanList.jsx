3401a557856018de01de87343265ffd7ed2b480e
supreetkaur <supreet97@gmail.com>
2023-12-28 11:33:16 -0800

    restructure kirtan list layout

--- src/components/KirtanList/KirtanList.jsx
+++ src/components/KirtanList/KirtanList.jsx
@@ -1,7 +1,8 @@
 import "./KirtanList.scss";
 // import InfiniteScroll from "react-infinite-scroll-component";
 import { Row } from "react-bootstrap";
-import { FileEarmarkPlay, Download } from "react-bootstrap-icons";
+import { Download } from "react-bootstrap-icons";
+// FileEarmarkPlay
 
 function KirtanList({
   searchTerm,
@@ -29,49 +30,38 @@ function KirtanList({
   }
 
   const handleKirtanSelection = (kirtan) => {
-    // console.log("kirtan", kirtan);
     setPlay(true);
     setSelectedKirtan(kirtan);
   };
 
   return (
-    <>
+    <section className="kirtan-list__container">
       {displayKirtans.map((kirtan) => {
-        // console.log(kirtan.Title);
         return (
-          <Row>
-            <div className="list-group">
-              <p
-                // eslint-disable-next-line
-                // href=""
+          <Row className="">
+            <div className="kirtan-list-item">
+              <div className="kirtan-list-item__container1">
+                <div className="kirtan-list-item__avatar"></div>
+              </div>
+              <div
+                className="kirtan-list-item__container2"
                 onClick={() => handleKirtanSelection(kirtan)}
-                className="list-group-item list-group-item-action flex-column align-items-start"
               >
-                <div className="d-flex w-100 justify-content-between">
-                  <p className="mb-1">{kirtan.Title}</p>
-                  <div className="d-flex justify-content-end">
-                    <small className="text-muted mr-1">
-                      <p onClick={() => handleKirtanSelection(kirtan)}>
-                        <FileEarmarkPlay className="button__play" />
-                      </p>
-                    </small>
-                    <small className="text-muted ml-1">
-                      <p onClick={() => handleKirtanSelection(kirtan)}>
-                        <Download className="button__download" />
-                      </p>
-                    </small>
-                  </div>
-                </div>
-                <p className="d-flex mb-1 ml-1">
+                <p className="kirtan-list-item__title">{kirtan.Title}</p>
+                <p className="kirtan-list-item__details">
                   {kirtan.Sevadar}, {kirtan.Album}
                 </p>
-                {/* <small className="text-muted">Donec id elit non mi porta.</small> */}
-              </p>
+              </div>
+              <div className="kirtan-list-item__container3">
+                <p onClick={() => handleKirtanSelection(kirtan)}>
+                  <Download className="button__download" />
+                </p>
+              </div>
             </div>
           </Row>
         );
       })}
-    </>
+    </section>
   );
 }
 
