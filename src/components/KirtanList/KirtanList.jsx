29f581e26956f18be2a69d947cb510d7c3737f50
supreetkaur <supreet97@gmail.com>
2023-12-28 14:56:51 -0800

    added unique key inside mapper

--- src/components/KirtanList/KirtanList.jsx
+++ src/components/KirtanList/KirtanList.jsx
@@ -56,7 +56,7 @@ function KirtanList({
     <section className="kirtan-list__container">
       {displayKirtans.map((kirtan) => {
         return (
-          <Row className="">
+          <Row key={kirtan.aid} className="">
             <div className="kirtan-list-item">
               <div className="kirtan-list-item__wrapper">
                 <div className="kirtan-list-item__container1">
