9d30f94d6036e2c71ae331a802e7cb91178e5cf1
supreetkaur <supreet97@gmail.com>
2023-12-28 15:10:04 -0800

    search bar styling

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,46 +1,29 @@
 import "./SearchBar.scss";
 // import Form from "react-bootstrap/Form";
-import { Col } from "react-bootstrap";
+// import { Col } from "react-bootstrap";
 import CloseButton from "../../assets/images/close-line-icon.png";
 
 function SearchBar({ inputRef, handleSearch, resetSearch }) {
   // searchHistory
   //   console.log("searchHistory", searchHistory);
   return (
-    <>
-      {/* <Form.Control
-        size="sm"
-        type="text"
-        name="searchInput"
-        ref={inputRef}
-        placeholder="Search by Kirtan, Name, Album"
-        onChange={handleSearch}
-      />
-      <img
-        src={CloseButton}
-        alt="clear search"
-        className="search__close-button"
-        onClick={resetSearch}
-      ></img> */}
-      <form className="searchbar">
-        {/* onSubmit={(event) => handleSearch(event)} */}
+    <div className="searchbar">
+      <form className="searchbar__form">
         <input
           name="searchInput"
           ref={inputRef}
-          className="searchbar__form"
+          className="searchbar__input"
           placeholder="Search by Kirtan, Name, Album"
           onChange={handleSearch}
         ></input>
-        {/* <button>Search</button> */}
+        <img
+          src={CloseButton}
+          alt="clear search"
+          className="searchbar__close-button"
+          onClick={resetSearch}
+        ></img>
       </form>
-      <Col xs={1}></Col>
-      <img
-        src={CloseButton}
-        alt="clear search"
-        className="searchbar__close-button"
-        onClick={resetSearch}
-      ></img>
-    </>
+    </div>
   );
 }
 
