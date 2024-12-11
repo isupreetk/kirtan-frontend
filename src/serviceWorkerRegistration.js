34cd7688660e22dac2e5b8a64a0834ea8cf1b40b
isupreetk <supreet97@gmail.com>
2024-12-11 15:02:29 -0800

    configures service worker to allow fetching different things from cache or network based on cache_preferred, cache_excluded, default strategies

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
   // console.log("Service worker is supported by browser");
-  navigator.serviceWorker.register("/service-worker.js")
+  navigator.serviceWorker.register("/service-worker.js", { scope: '/' })
   .then((res) => {console.log("Service worker is successfully registered!")})
   .catch((err) => {console.log("Error while registering service worker", err)})
 } else {
