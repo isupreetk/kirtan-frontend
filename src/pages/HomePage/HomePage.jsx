3560f7ff0c690f1545845cd4441df96ce0280711
isupreetk <supreet97@gmail.com>
2026-03-30 22:00:55 -0400

    uses version.json from brahmbungadodra.org

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -54,7 +54,8 @@ function HomePage() {
 
   const loadKirtans = () => {
     axios
-      .get(`${process.env.REACT_APP_API_URL}/settings?key=Version&key=FileURL`)
+      // .get(`${process.env.REACT_APP_API_URL}/settings?key=Version&key=FileURL`)
+      .get('https://brahmbungadodra.org/kirtanrecords/kirtancsv/version.json')
       .then((data) => {
         data.data.forEach((d) => {
           newDBInfo[d.key] = d.value;
